929a2a654e99dd6879cae2c9d2fbf5601e8591f9
afesta12 <andrewfesta11@yahoo.com>
2022-12-28 22:20:36 -0500

    Implemented user registration

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,70 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { signOut, onAuthStateChanged } from "firebase/auth"
+import { auth } from "../firebase"
+import { useNavigate } from "react-router-dom"
 
 export default function Home() {
+
+    // Nav
+    const navigate = useNavigate();
+
+    // Keep user from going to homepage when not logged in
+    useEffect(() => {
+
+        auth.onAuthStateChanged(user => {
+
+            // Redirect to login
+            if(!user) navigate('/')
+
+        })
+
+    }, [])
+
+    // Sign out -> redirect to home
+    const handleSignOut = () => {
+
+        signOut(auth).then(() => {
+
+            navigate('/');
+
+        }).catch(error => {alert(error.message)});
+
+
+    }
+
   return (
-    <div>Home</div>
+    <div className='container home'>
+    
+        <div className="container home-input">
+        
+            <div className="text">
+            
+                <h5>Preorder name:</h5>
+                <input type="text" placeholder='Add order...' />
+
+            </div>
+
+            <div className="buttons">
+            
+                <button>Add to List</button>
+                <button onClick={ handleSignOut } >Sign Out</button>
+            
+            </div>
+        </div>
+
+        <div className="container orders">
+
+            <div className="order-top">
+            
+                <h5>Your Preorders</h5>
+            
+            </div>
+
+            <div className="order-list"></div>
+        
+        
+        </div>
+    
+    </div>
   )
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth"
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth"
 import { auth } from "../firebase.js"
 import { useNavigate } from "react-router-dom";
 
@@ -8,6 +8,18 @@ export default function Login() {
     // User email password
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+
+    // Register info
+    const [registerInformation, setRegisterInformation] = useState({
+
+        email: '',
+        confirmEmail: '',
+        password: '',
+        confirmPassword: '',
+ 
+    })
+
+    const [isRegistering, setIsRegistering] = useState(false);
 
     const navigate = useNavigate();
 
@@ -39,6 +51,30 @@ export default function Login() {
 
     }
 
+    // Register
+    const handleRegister = () => {
+
+        if(registerInformation.email !== registerInformation.confirmEmail) {
+
+            alert("Please confirm that the entered e-mail addresses match.");
+            return;
+
+        } else if(registerInformation.password !== registerInformation.confirmPassword) {
+
+            alert("Please confirm that the entered passwords match.");
+            return;
+
+        }
+
+        createUserWithEmailAndPassword(auth, registerInformation.email, registerInformation.password)
+        .then(() => {
+
+            navigate('/home')
+
+        }).catch(err => alert(err.message))
+
+    };
+
     // Sign in when sign in is clicked and redirect to homepage if the user exists
     const handleSignIn = () => {
 
@@ -53,28 +89,69 @@ export default function Login() {
   return (
     <div className='container login'>
 
-        <h1 className="main-text">Preorder List</h1>
-
-        <div className="container input">
+        { isRegistering ? (
+            <>
+                <h1 className="main-text">Sign Up</h1>
+                <div className="container input register">
         
-            <input type="email" 
-                   placeholder='E-mail'
-                   value={email}
-                   onChange={ handleEmailChange } />
+                    <input type="email" 
+                        placeholder='E-mail'
+                        value={registerInformation.email}
+                        onChange={ (e) => setRegisterInformation({...registerInformation, email: e.target.value}) } />
 
-            <input type="password" 
-                   placeholder='Password'
-                   value={password}
-                   onChange={ handlePasswordChange } />
-            
-            </div>
+                    <input type="E-mail"
+                           placeholder='Confirm E-mail'
+                           value={registerInformation.confirmEmail}
+                           onChange={(e) => setRegisterInformation({...registerInformation, confirmEmail: e.target.value})} />
 
-            <div className="container bottom">
-            
-                <button onClick={ handleSignIn }>Sign In</button>
+                    <input type="password" 
+                        placeholder='Password'
+                        value={registerInformation.password}
+                        onChange={(e) => setRegisterInformation({...registerInformation, password: e.target.value})} />
 
-                <a href="">Create an account</a>
-        </div>
+                    <input type="password" 
+                           placeholder='Confirm Password'
+                           value={registerInformation.confirmPassword}
+                           onChange={(e) => setRegisterInformation({...registerInformation, confirmPassword: e.target.value})} />
+        
+                </div>
+
+                <div className="container bottom">
+        
+                    <button onClick={ handleRegister }>Create Account</button>
+                    <button className='register' onClick={ () => setIsRegistering(false) }>Go back</button>
+                </div>
+
+            </>
+        ) : 
+        
+        (
+            <>
+
+                <h1 className="main-text">Preorder List</h1>
+                <div className="container input">
+        
+                    <input type="email" 
+                        placeholder='E-mail'
+                        value={email}
+                        onChange={ handleEmailChange } />
+
+                    <input type="password" 
+                        placeholder='Password'
+                        value={password}
+                        onChange={ handlePasswordChange } />
+        
+                </div>
+
+                <div className="container bottom">
+        
+                    <button onClick={ handleSignIn }>Sign In</button>
+
+                    <button className='register' onClick={ () => setIsRegistering(true) }>Create an account</button>
+                </div>
+            </>
+
+        )}
     
     </div>
   )
