6954ef65fbe183b84805d6d1ebb01f226587e639
afesta12 <andrewfesta11@yahoo.com>
2022-12-29 15:44:50 -0500

    Write functionality

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,51 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { signOut, onAuthStateChanged } from "firebase/auth"
-import { auth } from "../firebase"
+import { auth, db } from "../firebase"
 import { useNavigate } from "react-router-dom"
+import { uid } from "uid"
+import { set, ref, onValue } from "firebase/database"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark, faPen } from '@fortawesome/free-solid-svg-icons'
 
 export default function Home() {
 
     // Nav
     const navigate = useNavigate();
 
+    // Order
+    const [order, setOrder] = useState("");
+
+    // Orders
+    const [orders, setOrders] = useState([]);
+
     // Keep user from going to homepage when not logged in
     useEffect(() => {
 
         auth.onAuthStateChanged(user => {
 
+            if(user) {
+
+                onValue(ref(db, `/${auth.currentUser.uid}`), snapshot => {
+
+                    setOrders([]);
+                    const data = snapshot.val();
+
+                    if(data !== null) {
+
+                        Object.values(data).map(order => {
+
+                            setOrders((old) => [...old, order])
+
+                        })
+
+                    }
+
+                })
+
+            }
+
             // Redirect to login
-            if(!user) navigate('/')
+            else if(!user) navigate('/')
 
         })
 
@@ -32,6 +63,22 @@ export default function Home() {
 
     }
 
+    // Add order to user's orders DB
+    const writeToDatabase = () => {
+
+        const uidd = uid();
+
+        set(ref(db, `/${auth.currentUser.uid}/${uidd}`), {
+
+            order: order,
+            uidd: uidd
+
+        })
+
+        setOrder("");
+
+    }
+
   return (
     <div className='container home'>
     
@@ -40,13 +87,16 @@ export default function Home() {
             <div className="text">
             
                 <h5>Preorder name:</h5>
-                <input type="text" placeholder='Add order...' />
+                <input type="text" 
+                       placeholder='Add order...'
+                       value={order}
+                       onChange={(e) => setOrder(e.target.value)} />
 
             </div>
 
             <div className="buttons">
             
-                <button>Add to List</button>
+                <button onClick={ writeToDatabase } >Add to List</button>
                 <button onClick={ handleSignOut } >Sign Out</button>
             
             </div>
@@ -60,7 +110,30 @@ export default function Home() {
             
             </div>
 
-            <div className="order-list"></div>
+            <div className="order-list">
+            
+                {
+
+                    orders.map(order => (
+
+                        <div className="order">
+                        
+                            <p>{order.order}</p>
+
+                            <div className="icons">
+                            
+                                <FontAwesomeIcon icon={faXmark} color="#45A29E" className='icon' />
+                                <FontAwesomeIcon icon={ faPen } color="#45A29E" size='xs' className='icon' />
+                            
+                            </div>
+                        
+                        </div>
+
+                    ))
+
+                }
+            
+            </div>
         
         
         </div>
