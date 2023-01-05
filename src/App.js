bc1892ae43e75866831b8613e13c1a1635358352
afesta12 <andrewfesta11@yahoo.com>
2023-01-04 23:16:38 -0500

    gh pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Login from "./components/Login"
 import Home from "./components/Home"
 import {
-    BrowserRouter as Router,
+    HashRouter as Router,
     Routes,
     Route,
 } from "react-router-dom"
